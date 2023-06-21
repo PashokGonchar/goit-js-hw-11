1d6aaf87d5d9246ae62f440361609ab38f54234c
Pavlo Honcharenko <pavelgonchar13@gmail.com>
2023-06-21 13:19:32 +0300

    start

--- src/get_pictures.js
+++ src/get_pictures.js
@@ -1,3 +1,16 @@
-const BASE_URL = 'https://pixabay.com/api';
-const API_KEY = '37603582-92143e20137f7c1d3caa92753';
+export default class NewGetPictures {
+  #BASE_URL = 'https://pixabay.com/api/';
+  #API_KEY = '37603582-92143e20137f7c1d3caa92753';
 
+  fetchPhotos() {
+   return fetch(
+      `${this.#BASE_URL}?key=${this.#API_KEY}&q=car&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=1`
+    ).then(response => {
+      if (!response.ok) {
+        throw Error(response.status);
+      }
+
+      return response.json();
+    });
+  }
+}
--- src/index.js
+++ src/index.js
@@ -2,3 +2,18 @@ import axios from 'axios';
 import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+
+import NewGetPictures from './get_pictures';
+
+const newGetPictures = new NewGetPictures();
+
+const searchFormEl = document.querySelector('#search-form');
+const galleryEl = document.querySelector('gallery');
+
+const handleSearchFormSubmit = event => {
+    event.preventDefault();
+
+    newGetPictures.fetchPhotos().then(data => console.log(data))
+}
+
+searchFormEl.addEventListener('submit', handleSearchFormSubmit)
