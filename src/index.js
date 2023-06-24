c61911d936a0adadd263e8d327faf42371fd536a
Pavlo Honcharenko <pavelgonchar13@gmail.com>
2023-06-24 08:49:55 +0300

    first_markup_done

--- src/index.js
+++ src/index.js
@@ -4,16 +4,20 @@ import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 import NewGetPictures from './get_pictures';
+import createPicturesCard from './templates/gallery-card.hbs'
 
 const newGetPictures = new NewGetPictures();
 
 const searchFormEl = document.querySelector('#search-form');
-const galleryEl = document.querySelector('gallery');
-
+const galleryEl = document.querySelector('.gallery');
 const handleSearchFormSubmit = event => {
-    event.preventDefault();
+  event.preventDefault();
 
-    newGetPictures.fetchPhotos().then(data => console.log(data))
-}
+  newGetPictures.fetchPhotos().then(data => {
+    // console.log(data.hits)
+    galleryEl.innerHTML = createPicturesCard(data.hits);
+  });
+};
 
-searchFormEl.addEventListener('submit', handleSearchFormSubmit)
+searchFormEl.addEventListener('submit', handleSearchFormSubmit);
+
