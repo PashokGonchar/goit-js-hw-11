e243d0c809ce53ec8b466e50cf3fdec5d89ae1a5
Pavlo Honcharenko <pavelgonchar13@gmail.com>
2023-06-26 12:16:23 +0300

    pause

--- src/get_pictures.js
+++ src/get_pictures.js
@@ -2,9 +2,16 @@ export default class NewGetPictures {
   #BASE_URL = 'https://pixabay.com/api/';
   #API_KEY = '37603582-92143e20137f7c1d3caa92753';
 
+  query = null;
+  page = 1;
+
   fetchPhotos() {
-   return fetch(
-      `${this.#BASE_URL}?key=${this.#API_KEY}&q=car&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=1`
+    return fetch(
+      `${this.#BASE_URL}?key=${this.#API_KEY}&q=${
+        this.query
+      }&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${
+        this.page
+      }`
     ).then(response => {
       if (!response.ok) {
         throw Error(response.status);
--- src/index.js
+++ src/index.js
@@ -4,20 +4,64 @@ import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 import NewGetPictures from './get_pictures';
-import createPicturesCard from './templates/gallery-card.hbs'
+import createPicturesCard from './templates/gallery-card.hbs';
 
 const newGetPictures = new NewGetPictures();
 
 const searchFormEl = document.querySelector('#search-form');
 const galleryEl = document.querySelector('.gallery');
+const inputEl = searchFormEl.firstElementChild;
+const loadMoreBtnEl = document.querySelector('.btn-primary');
+
 const handleSearchFormSubmit = event => {
   event.preventDefault();
 
-  newGetPictures.fetchPhotos().then(data => {
-    // console.log(data.hits)
-    galleryEl.innerHTML = createPicturesCard(data.hits);
-  });
+  if (!inputEl.value.trim()) {
+    return;
+  }
+
+  newGetPictures.query = inputEl.value;
+  inputEl.value = '';
+  galleryEl.innerHTML = '';
+
+  newGetPictures
+    .fetchPhotos()
+    .then(data => {
+      // console.log(data)
+      if (data.hits.length === 0) {
+        Notiflix.Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+
+        loadMoreBtnEl.classList.add('is-hidden');
+
+        return;
+      }
+
+      galleryEl.innerHTML = createPicturesCard(data.hits);
+
+      loadMoreBtnEl.classList.remove('is-hidden');
+    })
+    .catch(console.warn);
+};
+
+const handleLoadMore = () => {
+  newGetPictures.page += 1;
+
+  newGetPictures
+    .fetchPhotos()
+    .then(data => {
+      // console.log(data.totalHits)
+      // if (newGetPictures.page === data.totalHits) {
+      //   loadMoreBtnEl.classList.add('is-hidden');
+      // }
+      galleryEl.insertAdjacentHTML('beforeend', createPicturesCard(data.hits));
+
+      loadMoreBtnEl.classList.remove('is-hidden');
+    })
+    .catch(console.warn);
 };
 
 searchFormEl.addEventListener('submit', handleSearchFormSubmit);
 
+loadMoreBtnEl.addEventListener('click', handleLoadMore);
