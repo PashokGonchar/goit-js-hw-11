e0a8a551eeca55c7cb28202677abac7cb39c722a
Pavlo Honcharenko <pavelgonchar13@gmail.com>
2023-06-27 23:50:24 +0300

    axios_bad

--- src/get_pictures.js
+++ src/get_pictures.js
@@ -1,25 +1,42 @@
+import axios from 'axios';
+
 export default class NewGetPictures {
   #BASE_URL = 'https://pixabay.com/api/';
   #API_KEY = '37603582-92143e20137f7c1d3caa92753';
 
   query = null;
   page = 1;
-  perPage = 40
-  totalHits = 0
+  perPage = 40;
+  totalHits = 0;
 
-  fetchPhotos() {
-    return fetch(
-      `${this.#BASE_URL}?key=${this.#API_KEY}&q=${
-        this.query
-      }&image_type=photo&orientation=horizontal&safesearch=true&per_page=${this.perPage}&page=${
-        this.page
-      }`
-    ).then(response => {
-      if (!response.ok) {
-        throw Error(response.status);
-      }
+    fetchPhotos() {
+      return fetch(
+        `${this.#BASE_URL}?key=${this.#API_KEY}&q=${
+          this.query
+        }&image_type=photo&orientation=horizontal&safesearch=true&per_page=${
+          this.perPage
+        }&page=${this.page}`
+      ).then(response => {
+        if (!response.ok) {
+          throw Error(response.status);
+        }
 
-      return response.json();
-    });
+        return response.json();
+      });
+    }
   }
-}
+
+  // fetchPhotos() {
+  //   return axios.get(`${this.#BASE_URL}`, {
+  //     params: {
+  //       key: this.#API_KEY,
+  //       q: this.query,
+  //       image_type: photo,
+  //       orientation: horizontal,
+  //       safesearch: true,
+  //       page: this.page,
+  //       per_page: this.perPage,
+  //     },
+  //   });
+  // }
+
